05ee2ac77dca0b753cd28bb87679c4a1ff472c50
Tieming Ji <tjinmsuedu@gmail.com>
2018-11-29 11:56:42 -0600

    Add files via upload

--- R/InitialValue.R
+++ R/InitialValue.R
@@ -7,9 +7,9 @@
 #' @return initial.value Initial values for EM algorithm
 #' @export
 initial.value <- function(obs){
-    sd <- sd(obs[,1])/2
-    s1 <- which(obs[,1] > 2*sd)
-    s2 <- which(obs[,1] < -2*sd)
+    sd <- sd(obs[,1])
+    s1 <- which(obs[,1] > sd)
+    s2 <- which(obs[,1] < -sd)
     initial.state <- rep(0, length(obs[,1]))
     initial.state[s1] <- 1
     initial.state[s2] <- 2
@@ -18,12 +18,12 @@ initial.value <- function(obs){
     if(sum(initial.state==1)!=0 & sum(initial.state==1)!=1){
         tmp.mu.pos <- mean(obs[initial.state==1,1])
     }else{
-    	tmp.mu.pos=3*sd
+    	tmp.mu.pos=3/2*sd
     }
     if(sum(initial.state==2)!=0 & sum(initial.state==2)!=1){
     	tmp.mu.neg <- mean(obs[initial.state==2,1])
     }else{
-    	tmp.mu.neg= -3*sd
+    	tmp.mu.neg=-3/2*sd
     }
     mu.pos <- (tmp.mu.pos+abs(tmp.mu.neg))/2
     mu.neg <- (-abs(tmp.mu.pos)+tmp.mu.neg)/2
@@ -33,7 +33,7 @@ initial.value <- function(obs){
     
     # transition parameters.
     p0=p1=p2=1/3
-    rho <- 0.5
+    rho <- 0.05
     tran.p <- array(0.1, dim=c(3,3))
 	
 	return(initial.value=list(p0=p0, p1=p1, p2=p2,
